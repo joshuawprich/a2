a8613de78cc1d1abccb70ec2fd1c1c492cd979f3
Joshua <joshuawprichard@gmail.com>
2021-10-19 09:44:32 +1000

    Added some comments to code

--- routes/search.js
+++ routes/search.js
@@ -4,12 +4,14 @@ const axios = require('axios');
 const Analyzer = require('natural').SentimentAnalyzer;
 const stemmer = require('natural').PorterStemmer;
 
+// Keys for Twtter API
 const keys = {
     API_KEY: 'QmW1wnzCrOjNA51jCrSuS4KSR',
     API_SECRET_KEY: 'zf1FRVgegAVq4ZmlcQajaY98oIryeZZoQj7ZeyDfifTXeWudI8',
     BEARER_TOKEN:  'AAAAAAAAAAAAAAAAAAAAAH0xUwEAAAAAUfwjS4cTHSJ582JTRSgemuUN8wc%3DWRh5XT93aRmNNRcApsBOWLjGJLhx5hZJo72gXvC7fbeCs2zb14',
 }
 
+// Options for axios get request
 var options = {
     method: "GET",
     url: "https://api.twitter.com/2/tweets/search/recent?query=hey&max_results=10",
@@ -19,6 +21,7 @@ var options = {
 
 }
 
+// Alalyses text from tweets and provides a sentiment
 function AnalyzeText(text) {
     var analyzer = new Analyzer("English", stemmer, "afinn");
     var spltText = text.split(" ");
@@ -27,13 +30,12 @@ function AnalyzeText(text) {
 
 // Touch the twitter end point
 router.get('/', function(req, res) {
-  //res.render('index', { title: 'Express' });
+
   const query = (req.query.query);
 
   axios
     .request(options)
     .then(function (response) {
-        // res.status(200).send(response.data);
         let data = response.data.data[0].text;
         console.log(data);
         AnalyzeText(data);
