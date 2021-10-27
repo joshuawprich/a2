84a38b8eacbb88e17173939fe959df0ffbba5eb0
Joshua <joshuawprichard@gmail.com>
2021-10-27 10:40:22 +1000

    Tweets now stream to the client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var needle = require("needle");
+var WebSocket = require("ws");
+
+const streamUrl = "https://api.twitter.com/2/tweets/sample/stream";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Twitter" });
 });
 
 module.exports = router;
